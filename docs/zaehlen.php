ccdec841885045299af915c73ed1243bd0a277a1
Max Walter <walt@walt-dev.de>
2019-08-29 10:05:53 +0200

    Added some styles

--- docs/zaehlen.php
+++ docs/zaehlen.php
@@ -1,8 +1,27 @@
 <?php
 
-// echo 1 . '<br>';
-// echo 2 . '<br>';
-// echo 3 . '<br>';
+echo '<h1>Ohne Schleife</h1>';
+echo '<pre style="background: #e9edf0; padding: 30px">echo 1..10;</pre>';
+
+
+
+echo 1 . ' ';
+echo 2 . ' ';
+echo 3 . ' ';
+echo 4 . ' ';
+echo 5 . ' ';
+echo 6 . ' ';
+echo 7 . ' ';
+echo 8 . ' ';
+echo 9 . ' ';
+echo 10 .' ';
+
+
+
+
+
+
+
 echo '<h1>For Loop</h1></h1>';
 echo '<pre style="background: #e9edf0; padding: 30px">for($i = 1; $i <= 10; ++$i) echo $i;</pre>';
 
@@ -13,37 +32,37 @@ for($i = 1; $i <= 10; ++$i) echo $i . ' ';
 
 echo '<h1>Do While Loop</h1>';
 echo '<pre style="background: #e9edf0; padding: 30px">
-$a = 0;
+$i = 0;
 
 do {
-	echo $a;
-	++$a;
-} while ($a <= 10);
+	echo $i;
+	++$i;
+} while ($i <= 10);
 </pre>';
 
-$a = 0;
+$i = 0;
 
 do {
-	echo $a . ' ';
-	++$a;
-} while ($a <= 10);
+	echo $i . ' ';
+	++$i;
+} while ($i <= 10);
 
 
 echo '<h1>While Loop</h1>';
 
 echo '<pre style="background: #e9edf0; padding: 30px">
-$b = 0;
+$i = 0;
 
-while ($b <= 10) {
-	echo $b;
-	++$b;
+while ($i <= 10) {
+	echo $i;
+	++$i;
 }
 
 </pre>';
 
-$b = 0;
+$i = 0;
 
-while ($b <= 10) {
-	echo $b . ' ';
-	++$b;
+while ($i <= 10) {
+	echo $i . ' ';
+	++$i;
 }
