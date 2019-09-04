a61e9f31e2c8ad25dbc8daab1fbac13666f16413
Max Walter <walt@walt-dev.de>
2019-09-04 10:57:41 +0200

    Update fenster.php

--- docs/fenster.php
+++ docs/fenster.php
@@ -1,34 +1,37 @@
 <?php
 
+$Breite = 40;
+$Hoehe = 15;
 
-$Breite = 20;
-$Hoehe = 5;
-
-echo '<pre>******<br>';
-echo '*&nbsp;&nbsp;&nbsp;&nbsp;*<br>';
-echo '*&nbsp;&nbsp;&nbsp;&nbsp;*<br>';
-echo '*&nbsp;&nbsp;&nbsp;&nbsp;*<br>';
-echo '*&nbsp;&nbsp;&nbsp;&nbsp;*<br>';
-
-echo '****** <br></pre>';
-
+echo '<pre>';
 for($h = 1; $h <= $Hoehe; $h++) {
 	switch ($h) {
 		case '1':
 			for($w = 1; $w <= $Breite; ++$w) echo '*';
 			echo '<br>';
 		break;
-
 		case $Hoehe:
 			for($w = 1; $w <= $Breite; ++$w) echo '*';
 		break;
 		default:
 			echo '*';
 			$neueBreite = $Breite - 2;
-			for($w = 1; $w <= $neueBreite; ++$w) echo '&nbsp;&nbsp;';
-			echo '*<br>';
+			for($w = 1; $w <= $neueBreite; ++$w) echo '&nbsp;';
+			echo '*&#10;';
 		break;
 	}
 }
+echo '<br>';
+for($i = 1; $i <= $Breite; $i++) {
+	for($x = 0; $x <= $Breite -($i +1); ++$x) echo '&nbsp;';
+	echo '*&#10;';
+}
+
+for($i = 1; $i <= $Breite; $i++) {
+	for($a = $i - 2; $a >= 0; $a--) echo '&nbsp;';
+	
+	echo '*&#10;';
+}
+echo '</pre>';
  
 
