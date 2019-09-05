8e991ee7a972f38013444b8d3873a86ee48d7dea
Max Walter <walt@walt-dev.de>
2019-09-05 08:00:44 +0200

    better solution

--- docs/fenster.php
+++ docs/fenster.php
@@ -4,24 +4,22 @@ $Breite = 15;
 $Hoehe = 15;
 
 echo '<pre style="line-height: 7px">';
+
 for($h = 1; $h <= $Hoehe; $h++) {
-	switch ($h) {
-		case '1':
-			for($w = 1; $w <= $Breite; ++$w) echo '*';
-			echo '<br>';
-		break;
-		case $Hoehe:
-			for($w = 1; $w <= $Breite; ++$w) echo '*';
-		break;
-		default:
-			echo '*';
-			$neueBreite = $Breite - 2;
-			for($w = 1; $w <= $neueBreite; ++$w) echo '&nbsp;';
-			echo '*&#10;';
-		break;
+
+	if($h == '1' or $h == $Hoehe) {
+		for($w = 1; $w <= $Breite; ++$w) echo '*';
+		echo '<br>';
+	} else {
+		echo '*';
+		$neueBreite = $Breite - 2;
+		for($w = 1; $w <= $neueBreite; ++$w) echo '&nbsp;';
+		echo '*&#10;';
 	}
 }
+
 echo '<br>';
+
 for($i = 1; $i <= $Breite; $i++) {
 	for($x = 0; $x <= $Breite -($i +1); ++$x) echo '&nbsp;';
 	echo '*&#10;';
@@ -29,9 +27,9 @@ for($i = 1; $i <= $Breite; $i++) {
 
 for($i = 1; $i <= $Breite; $i++) {
 	for($a = $i - 2; $a >= 0; $a--) echo '&nbsp;';
-	
 	echo '*&#10;';
 }
+
 echo '</pre>';
  
 
